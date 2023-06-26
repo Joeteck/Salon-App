7300071da2df28c83a0f0f894e2d673e9de7ac93
joeteck <adeyojuibukunoluwa1@gmail.com>
2023-06-26 00:51:39 +0000

    User commit

---
+++ salon-app/src/components/Salon Navigation/index.js
@@ -0,0 +1,9 @@
+import BottomNav from './BottomNav';
+import NewService from './NewService';
+import SideNav from './SideNav';
+
+export{
+    BottomNav,
+    NewService,
+    SideNav,
+};  
---
+++ salon-app/src/components/User Navigation/index.js
@@ -0,0 +1,11 @@
+import BottomNav from './BottomNav';
+import Salon from './Salon';
+import Search from './Search';
+import SideNav from './SideNav';
+
+export{
+    BottomNav,
+    Salon,
+    SideNav,
+    Search,
+};
